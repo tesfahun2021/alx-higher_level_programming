2274f9026df51165df84518093c07b92a07703be
tesfahun2021 <tesfahunshamero10@gmail.com>
2021-11-22 22:33:34 +0300

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_cp = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    list_cp[idx] = element
+    return list_cp
